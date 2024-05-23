9e7d11a3118279a4105e8706f18f617a5a902065
GodKe <3601262198@qq.com>
2024-05-23 15:03:39 +0800

    update lab10 prework

--- 课件/14. 网络接口/TCPclient.py
+++ 课件/14. 网络接口/TCPclient.py
@@ -6,50 +6,54 @@ import time
 import sys
 
 # client program
-print ('######################### TCP client #########################')
+print("######################### TCP client #########################")
 
-# server's IP address and port:
-HOST_IP = '192.168.2.112'
-HOST_PORT = 50007
+# server的IP和端口:
+HOST_IP = "127.0.0.1"  # 与server端程序相同
+HOST_PORT = 8888  # 与server端程序相同
 
-# create socket object:
+# 创建socket:
 socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 i = 0
 while True:
     try:
-        # connect to server:
-        print ('connecting to server...')
+        # 连接server:  socket.connect(address)
+        print("connecting to server...")
         socket_tcp.connect((HOST_IP, HOST_PORT))
         break
     except Exception:
-        print ('fail, retry later...')
+        print("fail, retry later...")
         i += 1
+        # 失败5次, 放弃:
         if i == 5:
-            print ('fail!')
+            print("fail!")
             sys.exit(1)
+        # 否则, 1s后重试:
         time.sleep(1)
-        continue
-print ('succeed!')
+print("succeed!")
 
-# receive welcome from server:
+# 接收欢迎信息:
 data_recv = socket_tcp.recv(512)
-print ('received:', data_recv.decode())
+print("received:", data_recv.decode())
 
 while True:
     try:
-        data_send = input('message to send: ')
-        # send data to server:
+        data_send = input(
+            "message to send: "
+        )  # 注意python2中input和raw_input的区别, raw_input返回值为字符串
+        # 向server发送字符:
         socket_tcp.send(data_send.encode())
-        if data_send == 'exit':
+        # 发送exit, 结束会话:
+        if data_send == "exit":
             break
-        # receive echo from server:
+        # 否则, 从server接收回复:
         data_recv = socket_tcp.recv(512)
-        print ('received:', data_recv.decode())
+        print("received:", data_recv.decode())
     except Exception:
-        print ('error!')
+        print("error!")
         socket_tcp.close()
         sys.exit(1)
-    
-# close socket:
+
+# 关闭socket:
 socket_tcp.close()
--- 课件/14. 网络接口/TCPserver.py
+++ 课件/14. 网络接口/TCPserver.py
@@ -5,41 +5,42 @@ import socket
 import sys
 
 # server program
-print ('######################### TCP server #########################')
+print("######################### TCP server #########################")
 
-# server's IP address and port:
-HOST_IP = '192.168.2.109'
-HOST_PORT = 50007
+# server的IP和端口:
+HOST_IP = "127.0.0.1"  # 改为你的server(树莓派/PC)的IP地址！
+HOST_PORT = 8888  # 0-65536, 选择无特殊含义的即可
 
-# create socket object:
+# 创建socket:  socket.socket([family[, type[, proto]]]) -> socket, family: IP+port选AF_INET, type: TCP选SOCK_STREAM
 socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind socket to address:
+# 绑定IP和端口:  socket.bind(address)
 socket_tcp.bind((HOST_IP, HOST_PORT))
-# listen for connections:
+# 监听连接请求:  socket.listen(backlog), backlog: 连接队列最大长度
 socket_tcp.listen(1)
-print ('listen @ IP address:', HOST_IP, 'port:' ,HOST_PORT)
+print("listen @ IP address:", HOST_IP, "port:", HOST_PORT)
 
-# accept a connection:
+# 接受client的连接请求:  socket.accept() -> (conn, address), conn: 用来收发数据的socket对象
 socket_conn, (client_ip, client_port) = socket_tcp.accept()
-print ('connected by: IP address:', client_ip, 'port:', client_port)
-# send welcome to client:
-socket_conn.send(('Welcome!').encode())
+print("connected by: IP address:", client_ip, "port:", client_port)
+# 发送欢迎信息:  socket.send(string[, flags])
+socket_conn.send(("Welcome!").encode())
 
 while True:
     try:
-        # receive data from client:
+        # 从client接收字符:  socket.recv(bufsize[, flags])
         data = socket_conn.recv(512)
         if len(data) > 0:
-            print ('received:', data.decode())
-            if data.decode() == 'exit':
+            print("received:", data)
+            # 收到exit, 结束会话:
+            if data.decode() == "exit":
                 break
-            # send echo to client:
+            # 否则, 回复给client相同的字符:
             socket_conn.send(data)
     except Exception:
-        print ('error!')
+        print("error!")
         socket_tcp.close()
         sys.exit(1)
 
-# close socket:
+# 关闭socket:  socket.close()
 socket_conn.close()
 socket_tcp.close()
